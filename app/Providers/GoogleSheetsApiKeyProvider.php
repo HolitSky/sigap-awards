9d37f8021bd3c30b7805371961bbf1d9fb79de19
HolitSky <holit.singkawang12@gmail.com>
2025-10-13 14:06:20 +0700

    apt dashboard form bpkh dll

--- app/Providers/GoogleSheetsApiKeyProvider.php
+++ app/Providers/GoogleSheetsApiKeyProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Revolution\Google\Client\GoogleApiClient;
+use GuzzleHttp\Client as GuzzleClient;
 
 class GoogleSheetsApiKeyProvider extends ServiceProvider
 {
@@ -40,6 +42,36 @@ class GoogleSheetsApiKeyProvider extends ServiceProvider
         if ($range = env('GOOGLE_SHEETS_RANGE')) {
             config(['google.sheets.range' => $range]);
         }
+
+        // Configure CA bundle for SSL verification to avoid cURL error 60 on Windows/Laragon
+        $defaultLaragonCa = 'C:\\laragon\\etc\\ssl\\cacert.pem';
+        $envCa = env('GOOGLE_CA_BUNDLE') ?: env('SSL_CERT_FILE') ?: env('CURL_CA_BUNDLE');
+        $caBundle = $envCa ?: (file_exists($defaultLaragonCa) ? $defaultLaragonCa : null);
+
+        if ($caBundle && file_exists($caBundle)) {
+            // Ensure environment hints are present for Guzzle/cURL
+            @putenv('SSL_CERT_FILE='.$caBundle);
+            @putenv('CURL_CA_BUNDLE='.$caBundle);
+        }
+
+        // Optional override to disable SSL verify locally (NOT for production)
+        $disableVerify = filter_var(env('GOOGLE_DISABLE_SSL_VERIFY', false), FILTER_VALIDATE_BOOLEAN);
+
+        // After the GoogleApiClient is resolved, inject a Guzzle client with proper verify settings
+        $this->app->afterResolving(GoogleApiClient::class, function (GoogleApiClient $google) use ($caBundle, $disableVerify) {
+            $guzzleOptions = [
+                'base_uri' => \Google\Client::API_BASE_PATH,
+                'http_errors' => false,
+            ];
+
+            if ($disableVerify) {
+                $guzzleOptions['verify'] = false;
+            } elseif ($caBundle && file_exists($caBundle)) {
+                $guzzleOptions['verify'] = $caBundle;
+            }
+
+            $google->getClient()->setHttpClient(new GuzzleClient($guzzleOptions));
+        });
     }
 }
 
--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -12,6 +12,8 @@
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{ asset('/sigap-assets/images/favicon.ico') }}">
 
+        <base href="{{ url('/') }}/">
+
         <!-- DataTables -->
         <link href="{{ asset('/dashboard-assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('/dashboard-assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
